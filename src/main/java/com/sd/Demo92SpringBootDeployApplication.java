04007a988710147a80c4e2dfa205c4908f9074b2
Ajinkya More <ajinkyamore90@gmail.com>
2025-01-29 18:53:23 +0530

    commit...1

---
+++ src/main/java/com/sd/Demo92SpringBootDeployApplication.java
@@ -0,0 +1,13 @@
+package com.sd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo92SpringBootDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo92SpringBootDeployApplication.class, args);
+	}
+
+}
